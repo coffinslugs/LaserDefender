2c5b3c58ec7f43012d06785405340f1b7e0725ec
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-09-10 09:52:59 -0600

    added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Vector2 rawInput;
+    [SerializeField] float moveSpeed = 10f;
+
+    void Update()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
