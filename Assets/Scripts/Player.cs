5ca0b0643e7446cf163f927a431f9f72cc771404
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-09-11 06:33:29 -0600

    Added bounds to lock the player in, and some additional padding.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,39 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+    void Start()
+    {
+        InitBounds();
+    }
 
     void Update()
     {
         MovePlayer();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void MovePlayer()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPosition;
     }
 
     void OnMove(InputValue value)
